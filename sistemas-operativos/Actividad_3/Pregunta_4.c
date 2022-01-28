7dbb01948e5cc6624e2f0d7984b4cee481e16fa8
HappyFacell <is722820@iteso.mx>
2022-01-28 15:34:44 -0600

    Actividad 3 desde Rosewood

---
+++ sistemas-operativos/Actividad_3/Pregunta_4.c
@@ -0,0 +1,28 @@
+/*
+4.- Desarrolla un programa que solicite al 
+usuario una cadena de caracteres, esa cadena 
+será el nombre de un programa1 a ejecutarse, 
+una vez que este programa termine de ejecutarse 
+permitirá al usuario introducir otro programa a 
+ejecutarse, esto terminará cuando el usuario introduzca exit. 
+Para este ejercicio utiliza la llamada al sistema system(). 
+En este ejercicio estamos haciendo nuestro primer shell o 
+intérprete de comandos.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main()
+{
+    char cmd[80];
+
+    do
+    {
+        printf("Shell >");
+        scanf("%[^\n]%*c", cmd);
+        system(cmd);
+    } while (strcmp(cmd, "exit") != 0);
+    return 0;
+}

f71bc5d2d773d03a7dc81f92019d28a1b66e7634
HappyFacell <is722820@iteso.mx>
2022-02-06 11:29:20 -0600

    Correcciones de la practica 2 desde RoseWood

---
+++ sistemas-operativos/Test.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+int main(void) {
+    const char* filename = "signals.txt";
+
+    FILE* input_file = fopen(filename, "r");
+    if (!input_file)
+        exit(EXIT_FAILURE);
+
+    struct stat sb;
+    if (stat(filename, &sb) == -1) {
+        perror("stat");
+        exit(EXIT_FAILURE);
+    }
+
+    char* file_contents = malloc(sb.st_size);
+    fread(file_contents, sb.st_size, 1, input_file);
+
+    
+
+    fclose(input_file);
+	printf("%s\n", file_contents);
+    free(file_contents);
+
+    exit(EXIT_SUCCESS);
+}

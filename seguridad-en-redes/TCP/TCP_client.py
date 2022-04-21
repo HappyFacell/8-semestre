9c8b70bf63aba6226971bc23eddce0aa26e51c74
HappyFacell <is722820@iteso.mx>
2022-04-21 18:55:04 -0500

    TCP client for send a file

---
+++ seguridad-en-redes/TCP/TCP_client.py
@@ -0,0 +1,16 @@
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+
+def main() -> None:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        with open("./client/file_to_send.txt", "rb") as file:
+            for line in file.readlines():
+                s.sendall(line)
+
+
+if __name__ == "__main__":
+    main()

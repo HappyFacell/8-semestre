f3a0b3c2916529ad004f058fe2d34d2bda9fc377
HappyFacell <is722820@iteso.mx>
2022-04-21 19:40:09 -0500

    Changes

--- seguridad-en-redes/TCP/TCP_client.py
+++ seguridad-en-redes/TCP/TCP_client.py
@@ -1,16 +1,16 @@
 import socket
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+HOST = "127.0.0.1"
+PORT = 65432
 
 
 def main() -> None:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        with open("./client/file_to_send.txt", "rb") as file:
-            for line in file.readlines():
-                s.sendall(line)
+        with open("./client/file_to_send.txt", "rb") as file: # abrimos el archivo a enviar y le asignamos un alias
+            for line in file.readlines(): # Leemos el archivo linea por linea
+                s.sendall(line) # Enviamos las lineas
 
 
 if __name__ == "__main__":
-    main()
+    main() # Codigo usuario

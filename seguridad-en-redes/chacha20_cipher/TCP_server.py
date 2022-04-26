53427a025f0fd968154de338805f9f86c60e54ea
HappyFacell <is722820@iteso.mx>
2022-04-26 10:18:22 -0500

    TCP with cipher message

--- seguridad-en-redes/chacha20_cipher/TCP_server.py
+++ seguridad-en-redes/chacha20_cipher/TCP_server.py
@@ -14,14 +14,18 @@ def main() -> None:
         with conn:
             print(f"Connected by {addr}")
             file_data = b"" # Datos del archivo
+            file_data2 = b"" # Datos del archivo
             while True:
                 data = conn.recv(1024)
                 if not data:
                     break
                 file_data += Pseudo_random.encrypt2(data) # Guardamos los datos recibidos para escribirlos en el archivo de servidor
+                file_data2 += Pseudo_random.decrypt2(file_data)
 
-    with open("file_to_recive.txt", "wb") as file: # Creamos el archivo y escribimos en el
+    with open("file_to_recive_encrypt.txt", "wb") as file: # Creamos el archivo y escribimos en el
         file.write(file_data)
+    with open("file_to_recive_decrypt.txt", "wb") as file: # Creamos el archivo y escribimos en el
+        file.write(file_data2)
 
 
 if __name__ == "__main__":

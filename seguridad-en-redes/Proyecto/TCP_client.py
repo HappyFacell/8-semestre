cd5eadd8b9aeefafd528fe7210d7ac4692065978
HappyFacell <is722820@iteso.mx>
2022-05-14 10:52:16 -0500

    Proyecto acabado

--- seguridad-en-redes/Proyecto/TCP_client.py
+++ seguridad-en-redes/Proyecto/TCP_client.py
@@ -10,6 +10,7 @@ SEPARATOR = "<SEPARATOR>"
 host = "127.0.0.1"
 port = 65432
 
+
 def login():
     print("User: ")
     user = input(str())
@@ -18,21 +19,24 @@ def login():
     user_pwd = [user, pwd]
     return user_pwd
 
+
 def openFile() -> str:
     filename = askopenfilename()
     return filename
+
 
 if __name__ == "__main__":
 
     myInfo = login()
 
     myFileName = openFile()
-    
+
     with open(myFileName, 'rb') as fileDataToRead:
         message = fileDataToRead.read()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
 
-    s.sendall(f"{message}{SEPARATOR}{myInfo[0]}{SEPARATOR}{myInfo[1]}{SEPARATOR}{myFileName}".encode())
+    s.sendall(
+        f"{message}{SEPARATOR}{myInfo[0]}{SEPARATOR}{myInfo[1]}{SEPARATOR}{myFileName}".encode())
     data = s.recv(BUFFER_SIZE)
     print(data.decode())
--- seguridad-en-redes/Proyecto/TCP_server.py
+++ seguridad-en-redes/Proyecto/TCP_server.py
@@ -1,91 +1,87 @@
 import socket
-import json
-import Pseudo_random
-from base64 import b64decode, b64encode
 from tkinter import SEPARATOR
 import nacl.utils
+import nacl.secret
 from nacl.signing import SigningKey
 from nacl.signing import VerifyKey
-from tkinter import SEPARATOR
+
+_key = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
 
 USUARIOS_AUTENTICADOS = {
-    "user1@correo.mx": "12345",
-    "user2@correo.mx": "98765",
+    "test@test.mx": "12345",
+    "is722820@iteso.mx": "#HolaMundo12345",
 }
 
-f = open("secrets.txt", "w")
+f = open("secretInfo.txt", "w")
+
 
 def cifrarArchivo(informacionArchivo: bytes):
     # Encriptar el mensaje que nos ha enviado el cliente
     # llave
-    chacha20_key = nacl.utils.random()
-
-    # Chacha baby
-    # chachaCipher = ChaCha20.new(key=chacha20_key)
-
+    box = nacl.secret.SecretBox(_key)
     # Encriptar la informacion
-    texto_encriptado = Pseudo_random.encrypt2(informacionArchivo)
+    encrypted = box.encrypt(bytes(informacionArchivo, 'utf-8'))
+    # print(type(encrypted))
 
-
-    f = open("secrets.txt", "a")
-    f.write(f"Datos cifrados:\n {texto_encriptado}\n")
+    f = open("secretInfo.txt", "a")
+    f.write(f"Datos cifrados:\n {encrypted}\n")
     f.close()
-    
+
     # Regresar el archivo encriptado
-    return texto_encriptado
+    return encrypted
 
-def decifrarArchivo(infoArchivo:str, key:bytes):
+
+def decifrarArchivo(infoArchivo):
     try:
-        b64 = json.loads(infoArchivo)
-        cipherText = b64decode(b64['ciphertext'])
-        textoDecifrado = Pseudo_random.decrypt2(cipherText)
-        
-        f = open("secrets.txt", "a")
-        f.write(f"Texto decifrado: \n{textoDecifrado}\n")
-        f.close()
-        # print(type(textoDecifrado.decode('utf-8')))
+        # print(type(infoArchivo))
+        box = nacl.secret.SecretBox(_key)
+        desencrypt = box.decrypt(infoArchivo)
+        # print(desencrypt)
 
-        return textoDecifrado.decode('utf-8')
+        f = open("secretInfo.txt", "a")
+        f.write(f"Texto decifrado: \n{desencrypt}\n")
+        f.close()
+
+        return desencrypt
 
     except (ValueError, KeyError):
         print("Error en la desencriptacion del archivo")
-    
 
-def firmaDeArchivo(informacionEncriptada:str):
+
+def firmaDeArchivo(informacionEncriptada: str):
     sign_key = SigningKey.generate()
     infoFirmada = sign_key.sign(informacionEncriptada)
-    f = open("secrets.txt", "a")
+    f = open("secretInfo.txt", "a")
     f.write(f'Informacion firmada: \n {infoFirmada}\n')
     f.close()
     return infoFirmada, sign_key
 
 
-def verificacionFirmaArchivos(infoFirmada, key:SigningKey):
+def verificacionFirmaArchivos(infoFirmada, key: SigningKey):
     verify_key = VerifyKey(key.verify_key.encode())
-    # print(verify_key.verify(infoFirmada))
     res = verify_key.verify(infoFirmada)
-    f = open("secrets.txt", "a")
+    f = open("secretInfo.txt", "a")
     f.write(f'Verificar firma: \n {res} \n')
     f.close()
-    return res 
+    return res
 
 
 def login(usr: str, pwd: str):
-    file = open("bitacora.txt", "a")
+    file = open("users.txt", "a")
     if usr != "" and pwd != "":
         if usr in USUARIOS_AUTENTICADOS and pwd in USUARIOS_AUTENTICADOS.values():
             # Agregar quien entro a la bitacora
-            file.write(f"+ Entro el usuario: {usr}\n")
+            file.write(f"-I- Entro el usuario: {usr}\n")
             file.write("\n")
             return True
-    file.write(f"- Intento entrar el usuario: {usr}\n")
+    file.write(f"-E- Intento entrar el usuario: {usr}\n")
     file.write("\n")
     file.close()
     return False
 
 
 def bitacoraAccesos():
-    f = open("bitacora.txt", "r")
+    f = open("users.txt", "r")
     print(f.read())
     f.close()
 
@@ -103,12 +99,13 @@ if __name__ == "__main__":
     # Bind the socket with the address and port
     s.bind((host, port))
 
-    #Listen to the connection
+    # Listen to the connection
     s.listen()
-    print(f"[*] Listening as {host}:{port}")
+    # print(f"[*] Listening as {host}:{port}")
 
     # Block execution and waits for an incomming connection
     client_socket, address = s.accept()
+    print(f"Connected by {address}")
 
     # Read whatever the client sends
     everything = client_socket.recv(BUFFER_SIZE).decode()
@@ -121,17 +118,20 @@ if __name__ == "__main__":
     else:
         # Cifrar el archivo
         print(f'Arhico cifrado')
-        archivoCifrado, decrypt, key = cifrarArchivo(message)
+        # archivoCifrado = cifrarArchivo(message)
+
+        archivoCifrado = cifrarArchivo(message)
         client_socket.sendall(f'Archivo cifrado: \n{archivoCifrado}'.encode())
 
         # Decifrar el archivo
         print(f'Decifrar el archivo')
-        archivoDecifrado = decifrarArchivo(decrypt, key)
-        client_socket.sendall(f'Archivo cifrado: \n{archivoDecifrado}'.encode())
-        
+        archivoDecifrado = decifrarArchivo(archivoCifrado)
+        client_socket.sendall(
+            f'Archivo Decifrado: \n{archivoDecifrado}'.encode())
+
         # Firmar el archivo encriptado
         print("Firmar el archivo")
-        infoFirmada, key =  firmaDeArchivo(archivoCifrado)
+        infoFirmada, key = firmaDeArchivo(archivoCifrado)
 
         # Verificar la firma del arhivo
         print("Verificacion de firma")
